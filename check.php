9d1dc6253f623ddf83cc40aef738412aaa523d3d
Vova <volodiabodnarchyk2000@gmail.com>
2020-09-15 18:23:03 +0300

    second commit

--- check.php
+++ check.php
@@ -1,25 +1,25 @@
 <?php
 
- $login = filter_var(trim($_POST ['login']),
- FILTER_SANTIZE_STRING);
- $name = filter_var(trim($_POST ['name']),
- FILTER_SANTIZE_STRING);
- $pass = filter_var(trim($_POST ['pass']),
- FILTER_SANTIZE_STRING);
+ $login = filter_var(trim($_POST ['login'])
+);
+ $name = filter_var(trim($_POST ['name'])
+);
+ $pass = filter_var(trim($_POST ['pass'])
+);
 
- if (mb_strlen($login)) < 5 || mb_strlen($login) > 90 {
+if (mb_strlen($login) < 5 || mb_strlen($login) > 90 ){
      echo "Недопустима довжина логіну";
      exit();
- } else if (mb_strlen($name)) < 3 || mb_strlen($name) > 50 {
+ } else if (mb_strlen($name) < 3 || mb_strlen($name) > 50 ){
     echo "Недопустима довжина логіну";
     exit();
-    else if (mb_strlen($pass)) < 2 || mb_strlen($pass) > 6 {
+ }else if (mb_strlen($pass) < 2 || mb_strlen($pass) > 6 ){
         echo "Недопустима довжина логіну";
         exit();
-
-$mysql = new mysqli('localhost', 'root', '', 'register_bd');
+ } 
+$mysql = new mysqli('localhost', 'Volodia', '123456', 'register_bd');
 $mysql->query("INSERT INTO `users` (`id`, `login`, `pass`, `name`) VALUES('$login', '$pass', '$name')");
 
 $mysql->close();
 
-?>
+ ?>
